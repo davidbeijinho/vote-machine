e5e0a140bf31a7f231bee2520a2094ab7f26b5c1
David Beijinho <BEDA@xxxlgroup.com>
2020-01-24 16:57:41 +0100

    feat: turn leds on

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int LED_RED = D6;
+const int LED_YELLOW = D7;
+const int LED_GREEN = D8;
+
+void setup()
+{
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+
+  digitalWrite(LED_RED, HIGH);
+
+  analogWrite(LED_YELLOW, 255);
+
+  analogWrite(LED_GREEN, 255 / 4);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 }
